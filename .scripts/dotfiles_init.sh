9c3a654e5090146c5d1baf8e8a9b5f2b6592a8ce
JasonHallman <jason.hallman@aol.com>
2023-12-13 01:25:11 -0500

    Update dotfiles_init.sh

--- .scripts/dotfiles_init.sh
+++ .scripts/dotfiles_init.sh
@@ -8,7 +8,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles";
   else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv --parents {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} bash -c "mkdir --parents {} ; mv --parents {} .dotfiles-backup/{}"
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
