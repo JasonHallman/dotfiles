74e28701ef50adb1aa3c397b7e70e98c34a94c1c
JasonHallman <jason.hallman@aol.com>
2023-12-13 01:47:21 -0500

    Update dotfiles_init.sh

--- .scripts/dotfiles_init.sh
+++ .scripts/dotfiles_init.sh
@@ -11,7 +11,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles";
   else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 
 dotfiles checkout --recurse-submodules
