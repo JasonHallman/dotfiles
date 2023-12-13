38b13bf7121c6aa00a17229e2431d4831298ce4c
JasonHallman <jason.hallman@aol.com>
2023-12-13 01:28:09 -0500

    Update dotfiles_init.sh

--- .scripts/dotfiles_init.sh
+++ .scripts/dotfiles_init.sh
@@ -1,14 +1,18 @@
 git clone --recurse-submodules -j8 --bare https://github.com/JasonHallman/dotfiles.git $HOME/.cfg
+
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
+
 mkdir -p .dotfiles-backup
 dotfiles checkout
+
 if [ $? = 0 ]; then
   echo "Checked out dotfiles";
   else
     echo "Backing up pre-existing dot files.";
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} bash -c "mkdir --parents .dotfiles-backup/{} ; mv --parents {} $_"
 fi;
+
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
